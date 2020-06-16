a474df3e4ede7ae757b2a393d7b4383772a3a569
caiohmgithub <caio.martins@bandtec.com.br>
2020-06-15 22:25:45 -0300

    Atualização API - YOSHI

--- projeto-site/routes/leituras.js
+++ projeto-site/routes/leituras.js
@@ -12,11 +12,10 @@ router.get('/ultimas', function(req, res, next) {
 	console.log(`Recuperando as últimas ${limite_linhas} leituras`);
 	
 	const instrucaoSql = `select top ${limite_linhas} 
-						temperatura, 
-						umidade, 
-						momento,
-						FORMAT(momento,'HH:mm:ss') as momento_grafico 
-						from leitura order by id desc`;
+						emissão, 
+						lux,
+						FORMAT(emissão,'HH:mm:ss') as momento_grafico 
+						from dados_sensor order by idDados desc`;
 
 	sequelize.query(instrucaoSql, {
 		model: Leitura,
@@ -37,8 +36,8 @@ router.get('/tempo-real', function (req, res, next) {
 	
 	console.log(`Recuperando a última leitura`);
 
-	const instrucaoSql = `select top 1 temperatura, umidade, FORMAT(momento,'HH:mm:ss') as momento_grafico  
-						from leitura order by id desc`;
+	const instrucaoSql = `select top 1 emissão, lux, FORMAT(emissão,'HH:mm:ss') as momento_grafico  
+						from dados_sensor order by idDados desc`;
 
 	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
 		.then(resultado => {
