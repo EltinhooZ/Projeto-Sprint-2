2a5b8040168b673e441e6b5d1e69bab1fcae191e
EltinhooZ <elton.silva@bandtec.com>
2020-06-24 13:34:24 -0300

    Alterações na api marise

--- projeto-site/config/config.js
+++ projeto-site/config/config.js
@@ -1,31 +1,30 @@
 module.exports = {
-  development: {
-    dialect: "sqlite",
-    storage: "./db.development.sqlite"
-  },
-  test: {
-    dialect: "sqlite",
-    storage: ":memory:"
-  },
-  production: {
-    username: 'adminlocal',
-    password: '20020302@Heric',
-    database: 'bdLuminosidade1',
-    host: 'serveruminosidade1a.database.windows.net',
-    dialect: 'mssql',
-    xuse_env_variable: 'DATABASE_URL',
-    dialectOptions: {
-      options: {
-        encrypt: true
-      }
+    development: {
+        dialect: "sqlite",
+        storage: "./db.development.sqlite"
     },
-    pool: { 
-      max: 5,
-      min: 1,
-      acquire: 5000,
-      idle: 30000,
-      connectTimeout: 5000
+    test: {
+        dialect: "sqlite",
+        storage: ":memory:"
+    },
+    production: {
+        username: 'adminlocal',
+        password: '20020302@Heric',
+        database: 'bdLuminosidade1',
+        host: 'serveruminosidade1a.database.windows.net',
+        dialect: 'mssql',
+        xuse_env_variable: 'DATABASE_URL',
+        dialectOptions: {
+            options: {
+                encrypt: true
+            }
+        },
+        pool: {
+            max: 5,
+            min: 1,
+            acquire: 5000,
+            idle: 30000,
+            connectTimeout: 5000
+        }
     }
-  }
 };
- 
--- projeto-site/routes/leituras.js
+++ projeto-site/routes/leituras.js
@@ -5,57 +5,57 @@ var Leitura = require('../models').Leitura;
 
 /* Recuperar as últimas N leituras */
 router.get('/ultimas', function(req, res, next) {
-	
-	// quantas são as últimas leituras que quer? 8 está bom?
-	const limite_linhas = 7;
 
-	console.log(`Recuperando as últimas ${limite_linhas} leituras`);
-	
-	const instrucaoSql = `select top ${limite_linhas} 
+    // quantas são as últimas leituras que quer? 8 está bom?
+    const limite_linhas = 300;
+
+    console.log(`Recuperando as últimas ${limite_linhas} leituras`);
+
+    const instrucaoSql = `select top ${limite_linhas} 
 						emissão, 
 						lux,
 						FORMAT(emissão,'HH:mm:ss') as momento_grafico 
 						from dados_sensor order by idDados desc`;
 
-	sequelize.query(instrucaoSql, {
-		model: Leitura,
-		mapToModel: true 
-	  })
-	  .then(resultado => {
-			console.log(`Encontrados: ${resultado.length}`);
-			res.json(resultado);
-	  }).catch(erro => {
-			console.error(erro);
-			res.status(500).send(erro.message);
-	  });
+    sequelize.query(instrucaoSql, {
+            model: Leitura,
+            mapToModel: true
+        })
+        .then(resultado => {
+            console.log(`Encontrados: ${resultado.length}`);
+            res.json(resultado);
+        }).catch(erro => {
+            console.error(erro);
+            res.status(500).send(erro.message);
+        });
 });
 
 
 // tempo real (último valor de cada leitura)
-router.get('/tempo-real', function (req, res, next) {
-	
-	console.log(`Recuperando a última leitura`);
+router.get('/tempo-real', function(req, res, next) {
 
-	const instrucaoSql = `select top 1 emissão, lux, FORMAT(emissão,'HH:mm:ss') as momento_grafico  
+    console.log(`Recuperando a última leitura`);
+
+    const instrucaoSql = `select top 1 emissão, lux, FORMAT(emissão,'HH:mm:ss') as momento_grafico  
 						from dados_sensor order by idDados desc`;
 
-	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
-		.then(resultado => {
-			res.json(resultado[0]);
-		}).catch(erro => {
-			console.error(erro);
-			res.status(500).send(erro.message);
-		});
-  
+    sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
+        .then(resultado => {
+            res.json(resultado[0]);
+        }).catch(erro => {
+            console.error(erro);
+            res.status(500).send(erro.message);
+        });
+
 });
 
 
 // estatísticas (max, min, média, mediana, quartis etc)
-router.get('/estatisticas', function (req, res, next) {
-	
-	console.log(`Recuperando as estatísticas atuais`);
+router.get('/estatisticas', function(req, res, next) {
 
-	const instrucaoSql = `select 
+    console.log(`Recuperando as estatísticas atuais`);
+
+    const instrucaoSql = `select 
 							max(temperatura) as temp_maxima, 
 							min(temperatura) as temp_minima, 
 							avg(temperatura) as temp_media,
@@ -64,14 +64,14 @@ router.get('/estatisticas', function (req, res, next) {
 							avg(umidade) as umidade_media 
 						from leitura`;
 
-	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
-		.then(resultado => {
-			res.json(resultado[0]);
-		}).catch(erro => {
-			console.error(erro);
-			res.status(500).send(erro.message);
-		});
-  
+    sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
+        .then(resultado => {
+            res.json(resultado[0]);
+        }).catch(erro => {
+            console.error(erro);
+            res.status(500).send(erro.message);
+        });
+
 });
 
 
